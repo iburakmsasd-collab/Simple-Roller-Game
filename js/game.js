99caeb9d7188de3e88b61e447300450031453857
iburakmsasd-collab <iburak_msasd@magnoliascience.org>
2026-09-16 14:37:48 -0700

    FIX YES

--- js/game.js
+++ js/game.js
@@ -18,7 +18,7 @@ Game.startLevel = function (levelNumber) {
   Game.levelNumber = levelNumber;
   Level.build(levelNumber);
   Enemies.reset();  
-  add Blaster.reset();.
+  Blaster.reset();.
   Player.reset();
   Game.mode = "playing";
   Game.showMessage("");
